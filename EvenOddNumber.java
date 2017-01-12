c4bff351eaf9af85dff0377bb0f20eb7cf7d3d1d
Sandip Khillare <sandipkhillare2@gmail.com>
2017-01-12 21:06:06 +0530

    Program for even odd number
    Program for even odd number

---
+++ EvenOddNumber.java
@@ -0,0 +1,16 @@
+class EvenOddNumber
+{
+	public static void main(String args[])
+	{
+		int n=10;
+		if(n%2==0)
+		{
+			System.out.println("Even Number");
+		}	
+		else
+		{
+		
+			System.out.println("Odd Number");
+		}
+	}
+}
