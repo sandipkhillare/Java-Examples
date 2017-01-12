2fd84d90b43525c2fff20e70543c437a3e8b3efb
Sandip Khillare <sandipkhillare2@gmail.com>
2017-01-12 21:10:45 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,7 +4,7 @@ class Fibonacci
 {
 	public static void main(String args[])
 	{
-		int num1=0;num2=,num3;
+		int num1=0;num2=1,num3;			//num1 and num2 assigned 0 and 1 respectively
 		int i, count=10;
 		System.out.println(num1+" "+num2);
 		for(i=2;i<count;++i)
