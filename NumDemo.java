42db0eb26a7753e2bb4726472693cbccb1e86d20
Sandip Khillare <sandipkhillare2@gmail.com>
2017-01-14 15:51:57 +0530

    Print 1 to 10 numbers using loop
    Print 1 to 10 numbers using loop

---
+++ NumDemo.java
@@ -0,0 +1,17 @@
+class NumDemo
+
+{
+    
+	public static void main(String args[])
+
+   	{
+        
+		int i;
+        
+		for(i=1;i<=10;i++)
+  
+          		System.out.println(i);
+  
+  	}
+
+}
