464abbf35a60519c983da2c67904a2663adbae9c
Sandip Khillare <sandipkhillare2@gmail.com>
2017-01-14 16:51:12 +0530

    Even Odd Number Without Modulus
    Even Odd Number Without Using Modulus operator

---
+++ EvenOddNum.java
@@ -0,0 +1,16 @@
+class EvenOddNum
+{
+	public static void main(String args[])
+	{
+		int n=10;
+		if((n/2)*2==0)
+		{
+			System.out.println("Even Number");
+		}	
+		else
+		{
+		
+			System.out.println("Odd Number");
+		}
+	}
+}
